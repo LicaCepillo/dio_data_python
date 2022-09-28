b52d7ff645c2ed09a71906ae6aa16524bc94b996
Lilian Lima <liliancepillo@gmail.com>
2022-09-29 00:06:41 +0100

    Update string_2.py

--- string_2.py
+++ string_2.py
@@ -4,3 +4,8 @@ profissao = "Data Analyst"
 linguagem = "Python"
 
 print("Nome: %s Idade: %s" % (nome, idade))
+print("Nome: {} Idade: {}".format(nome,idade))
+print("Nome: {1} Idade: {0}".format (idade, nome))
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome))
+print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+print("Nome: {name} Idade: {age}".format(age=idade, name=nome))
