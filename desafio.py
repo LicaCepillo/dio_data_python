1cb0f44e1d28e059179975bab9797bde91c00206
Lilian Lima <liliancepillo@gmail.com>
2022-09-29 01:18:19 +0100

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -4,6 +4,6 @@ distancia_towers = int(input("Digite a distância entre as torres ( de 0 a 10000
 diametro_saruman_tower = int(input("Digite o diametro da torre de Saruman (de 0 a 100) : "))
 diametro_sauron_tower = int(input("Digite o diametro da torre de Sauron (de 0 a 100) : "))
 diametros_towers = diametro_saruman_tower + diametro_sauron_tower
-imc = str(distancia_towers/diametros_towers)
+imc = (distancia_towers/diametros_towers)
 
-print(imc)
+print(f"IMC {imc:10.2f}")
