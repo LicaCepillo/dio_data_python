b56684c50992f1047cb6a784a314ce6322b2729e
Lilian Lima <liliancepillo@gmail.com>
2022-09-29 01:18:08 +0100

    Create desafio_2.py

---
+++ desafio_2.py
@@ -0,0 +1,10 @@
+##valores = input().split() 
+hot_dogs = 840
+participantes = 11
+
+# Calcule a média de cachorros quentes consumidas por participante e imprima o valor com DUAS casas decimais.
+media_cachorro = (hot_dogs/participantes)
+print(f"total {media_cachorro:10.2f}")
+
+
+
