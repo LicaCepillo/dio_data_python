f22b36834a9868558dbd0217200ee53d579d7447
Lilian Lima <liliancepillo@gmail.com>
2022-10-05 22:51:46 +0100

    Update lista.py

--- listas_python.py/lista.py
+++ listas_python.py/lista.py
@@ -8,12 +8,39 @@ print(frutas)
 
 letras = list("python")
 print(letras)
+
 numeros = list(range(10))
 print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, "São Paulo", True]
 print(carro)
 
+#fatiamento de listas
+
 frutas = ["maçã", "laranja","uva", "pera"]
 print(frutas [0] ) #maçã
 print(frutas [2] ) #uva
+
+#indices negativos
+frutas = ["maçã", "laranja","uva", "pera"]
+print(frutas [-1] ) #pera
+print(frutas [-2] ) #uva
+
+#armazenar objetos em listas [],append
+lista = []
+lista.append(1)
+lista.append("Python")
+lista.append([40,30,20])
+print(lista)
+
+# Limpar a lista
+lista = (1, "Python", [40,30,20])
+##print(lista)
+lista.clear()
+print(lista)
+
+#Retorna uma lista igual com isntancia diferente [].copyright()
+lista1 = (1, "Python", [40,30,50])
+l2= lista1.copy()
+print(lista1)
+
