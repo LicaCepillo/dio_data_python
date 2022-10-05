e764116a36338acefb4b1398ddada0a34e0f519b
Lilian Lima <liliancepillo@gmail.com>
2022-10-05 19:54:24 +0100

    Create lista.py

---
+++ listas_python.py/lista.py
@@ -0,0 +1,9 @@
+#Declaração de listas
+
+frutas =["Laranja","Maçã","Uva"]
+frutas = []
+
+letras = list("python")
+numeros = list(range(10))
+
+carro = ["Ferrari", "F8", 4200000, 2020, "São Paulo", True]
