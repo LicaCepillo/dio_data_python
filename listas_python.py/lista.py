9ab64517ca8c32698ccc8197dfcad5e4c17e586e
Lilian Lima <liliancepillo@gmail.com>
2022-10-05 19:58:56 +0100

    Update lista.py

--- listas_python.py/lista.py
+++ listas_python.py/lista.py
@@ -1,9 +1,19 @@
 #Declaração de listas
 
 frutas =["Laranja","Maçã","Uva"]
+print(frutas)
+
 frutas = []
+print(frutas)
 
 letras = list("python")
+print(letras)
 numeros = list(range(10))
+print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, "São Paulo", True]
+print(carro)
+
+frutas = ["maçã", "laranja","uva", "pera"]
+print(frutas [0] ) #maçã
+print(frutas [2] ) #uva
