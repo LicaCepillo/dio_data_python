f15755934851880bf01bd5793626b9b8163f8201
Lilian Lima <liliancepillo@gmail.com>
2022-10-06 14:59:09 +0100

    Update 01_desafio_bicicletaria.py

--- classes_objetos/01_desafio_bicicletaria.py
+++ classes_objetos/01_desafio_bicicletaria.py
@@ -1,11 +1,3 @@
-class Bicicleta:
 
-    def  __imit__ (self, cor, modelo, ano, valor):
-        self.cor = cor
-        self.modelo = modelo
-        self.ano = ano
-        self.valor = Valor
-
-b1 = Bicicleta("vermelha","Caloi", 2022, 189)
 
  
