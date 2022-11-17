a7c7961881c92a7848897aba7d5de44f87a0c3a9
Lilian Lima <liliancepillo@gmail.com>
2022-11-17 16:18:02 +0000

    funcoes python

---
+++ package-template-master/args_kargs.py
@@ -0,0 +1,7 @@
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.itens()])
+    print(mensagem)
+
+
+exibir_poema(data_extenso)
